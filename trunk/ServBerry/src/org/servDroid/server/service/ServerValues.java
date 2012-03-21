c063fe4a8184fcc9a6861141e4e09a5bb5add06c
joanpuigsanz <joanpuigsanz@00d629fc-1f9a-7d08-4882-23562782cbd8>
2012-03-21 20:58:57 +0000

    change name to servMart

--- trunk/ServBerry/src/org/servDroid/server/service/ServerValues.java
+++
@@ -1,8 +0,0 @@
-package org.servDroid.server.service;
-
-public interface ServerValues {
-	/** The Server is running */
-	public static final int STATUS_RUNNING = 5435;
-	/** The Server is stopped */
-	public static final int STATUS_STOPED = 5435 + 1;
-}
