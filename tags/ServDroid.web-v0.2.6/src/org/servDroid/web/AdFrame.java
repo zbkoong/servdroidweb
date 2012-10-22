f1f904d2f8e4efaa5c1d3f10354714a2f0b14d59
joanpuigsanz <joanpuigsanz@00d629fc-1f9a-7d08-4882-23562782cbd8>
2012-10-22 19:34:00 +0000

    Create tag for v0.2.6

---
+++ tags/ServDroid.web-v0.2.6/src/org/servDroid/web/AdFrame.java
@@ -0,0 +1,37 @@
+package org.servDroid.web;
+
+import org.servDroid.preference.AccessPreferences;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdFrame {
+
+	private static AdView adView;
+	private static String MY_AD_UNIT_ID = null;
+
+	public static void load(ViewGroup layout, Activity context) {
+		if (AccessPreferences.getShowAds() && MY_AD_UNIT_ID != null) {
+			if (adView == null) {
+				adView = new AdView(context, AdSize.BANNER, MY_AD_UNIT_ID);
+				adView.setVisibility(View.VISIBLE);
+				layout.setVisibility(View.VISIBLE);
+				layout.invalidate();
+
+				AdRequest request = new AdRequest();
+				adView.loadAd(request);
+				layout.addView(adView);
+			}
+		} else if (adView != null) {
+			adView.setVisibility(View.INVISIBLE);
+			layout.setVisibility(View.INVISIBLE);
+			layout.invalidate();
+		}
+	}
+
+}
